6812c8ff0c5b519f7453fbefccb2f8f9cb7308bd
whdgjs7300 <112137464+whdgjs7300@users.noreply.github.com>
2023-03-04 23:37:16 +0900

    3/4 reduxLogger

--- redux/src/store.jsx
+++ redux/src/store.jsx
@@ -1,22 +1,33 @@
 const redux = require('redux');
+const reduxLogger = require('redux-logger');
+const createStore = redux.createStore;
+const applyMiddleware = redux.applyMiddleware;
+const logger = reduxLogger.createLogger();
 
 // actions
 // action-type
 const ADD_SUBSCRIBER = 'ADD_SUBSCRIBER'
+const ADD_VIEWCOUNT = 'ADD_VIEWCOUNT';
 const addSubscriber = () => {
     return {
         type : ADD_SUBSCRIBER
     }
     
 }
+const addViewCount = () => {
+    return {
+        type : ADD_VIEWCOUNT
+    }
+    
+}
 
 
 // reducers
-const initialState = {
+const subscriberState = {
     subscribers : 365
 }
 
-const reducer = (state=initialState, action) => {
+const subscriberReducer = (state=subscriberReducer, action) => {
     switch(action.type) {
         case ADD_SUBSCRIBER :
             return {
@@ -26,7 +37,28 @@ const reducer = (state=initialState, action) => {
             default : return state;
     }
 }
+const viewState = {
+    viewCount : 100
+}
+const viewReducer = (state=viewState, action)=> {
+    switch(action.type){
+        case ADD_VIEWCOUNT :
+            return {
+                ...state,
+                viewCount: state.viewCount + 1
+            }
+            default : return state
+    } 
+}
+
 
 // store
+const store = createStore(subscriberReducer, applyMiddleware(logger));
 
 // subscribe - view - dispatch
+
+store.subscribers(()=>{
+    
+})
+
+export default store;
