768f6e72825fad4c60c7e620a793774ffea630c6
whdgjs7300 <112137464+whdgjs7300@users.noreply.github.com>
2023-03-06 19:40:55 +0900

    3/6 reduxTodo

--- redux/src/App.js
+++ redux/src/App.js
@@ -4,16 +4,25 @@ import Subscribers from './components/Subscribers';
 import {Provider} from 'react-redux';
 import store from './redux/store';
 import Display from './components/Display';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './page/Home';
+import Detail from './page/Detail';
 
 function App() {
   return (
+    <BrowserRouter>
     <Provider store={store}>
       <div className="App">
-      <Subscribers/>
-      <Display/>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/:id' element={<Detail/>}></Route>
+      </Routes>
+
     </div>
 
     </Provider>
+    </BrowserRouter>
+    
     
   );
 }
---
+++ redux/src/page/Home.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+const Home = () => {
+    const [text, setText] = useState('');
+    const onChange = (e) => {
+        setText(e.target.value);
+    }
+    const onSubmit = (e) => {
+        e.preventDefault();
+        // 서밋한 후 빈 값 만들기
+        setText('');
+    }
+
+    return ( 
+        <div>
+            <h1>To Do</h1>
+            <form action="" onSubmit={onsubmit}>
+                <input type="text"  value={text} onChange={onChange}/>
+                <button>ADD</button>
+            </form>
+            <ul>
+
+            </ul>
+        </div>
+    );
+}
+
+export default Home;
